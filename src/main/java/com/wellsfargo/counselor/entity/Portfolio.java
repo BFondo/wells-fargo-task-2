b9ff5d9a97018dd37caf761f21154022b9196ec3
Becca Fondo <Rfondo@charlotte.edu>
2025-12-03 16:00:33 -0500

    Added Advisor, Client, Portfolio and Security classes

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,30 @@
+package com.wellsfargo.counselor.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @ForignKey 
+    private long clientId;
+
+    @Column(nullable = false)
+    private String creationDate;
+
+    protected Portfolio() {
+
+    }
+
+    public Portfolio(long clientId, String creationDate) {
+        this.clientId = clientId;
+        this.creationDate = creationDate;
+    }
+}
